ab29cf0f699a86e26b27b6f72a11bb97f35d5202
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-06-08 22:39:15 -0300

    add: add / route to login page. Modified file: App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <main>
+      <div>Hello, TrybeWallet!</div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={ Login }
+        />
+      </Switch>
+    </main>
+  );
 }
 
 export default App;
