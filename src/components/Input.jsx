c254795bbddf57cea3fac53e74cc71e55d985cdc
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-06-10 17:25:20 -0300

    add: create Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Input extends Component {
+  render() {
+    const { type, name, label/* , onChange, dataTestidInfo */ } = this.props;
+    const id = `${name}-input`;
+    // console.log(`onChange: ${(onChange) ? 'oi' : 'undef'}`);
+    const dataTestid = id;
+    const dataTestidLabel = `${id}-label`;
+    // const dataTestidInfo = {
+    //   exist: true,
+    //   value: ''
+    // };
+
+    return (
+      <label
+        htmlFor={ id }
+        data-testid={ dataTestidLabel }
+      >
+        {`${label}:`}
+        <input
+          id={ id }
+          type={ type }
+          name={ name }
+          data-testid={ dataTestid }
+        />
+      </label>
+    );
+  }
+}
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+}.isRequired;
+
+export default Input;
