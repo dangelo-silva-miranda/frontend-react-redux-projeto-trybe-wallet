1ba627410bf0c2ae32eb4a0f89819bee8ba9a59b
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-06-17 00:07:39 -0300

    feat: :sparkles: implements currencies and add expense case in wallet reducer

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -2,36 +2,62 @@ import {
   REQUEST_CURRENCIES,
   REQUEST_CURRENCIES_SUCCESS,
   REQUEST_CURRENCIES_ERROR,
+  ADD_EXPENSE,
 } from '../actions';
 
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 
 const initialState = {
-  currencies: [],
+  currencies: {},
   expenses: [],
+  isFetching: false,
+  totalExpenses: 0,
 };
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-  case REQUEST_CURRENCIES:
+  case REQUEST_CURRENCIES: {
+    const { isFetching } = payload;
     return {
       ...state,
-      isFetching: payload.isFetching,
+      isFetching,
     };
+  }
 
-  case REQUEST_CURRENCIES_SUCCESS:
+  case REQUEST_CURRENCIES_SUCCESS: {
+    const { isFetching, currencies } = payload;
+    // const currenciesArray = Object.keys(currencies)
+    //   .reduce(
+    //     (acc, currency) => {
+    //       acc.push({ [currency]: currencies[currency] });
+    //       return acc;
+    //     }, [],
+    //   );
     return {
       ...state,
-      isFetching: payload.isFetching,
-      currencies: payload.currencies,
+      isFetching,
+      currencies/* : currenciesArray */,
     };
+  }
 
-  case REQUEST_CURRENCIES_ERROR:
+  case REQUEST_CURRENCIES_ERROR: {
+    const { isFetching, error } = payload;
     return {
       ...state,
-      isFetching: payload.isFetching,
-      error: payload.error,
+      isFetching,
+      error,
     };
+  }
+
+  case ADD_EXPENSE: {
+    const { expense, expense: { exchangeRates, currency, value } } = payload;
+    const totalExpense = exchangeRates[currency].ask * value;
+    return {
+      ...state,
+      expenses: [...state.expenses, expense],
+      totalExpenses: state.totalExpenses + totalExpense,
+    };
+  }
 
   default:
     return state;
